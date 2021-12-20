ecfa9a891aefab655460cf5f439f337cceed3258
rewq985 <bnbt3@naver.com>
2021-12-21 06:09:52 +0900

    v1: 프론트 컨트롤러를 도입 기존 구조를 최대한 유지하면서 프론트 컨트롤러를 도입 v2: View 분류 단순 반복 되는 뷰 로직 분리 v3: Model 추가 서블릿 종속성 제거 뷰 이름 중복 제거 v4: 단순하고 실용적인 컨트롤러 v3와 거의 비슷 구현 입장에서 ModelView를 직접 생성해서 반환하지 않도록 편리한 인터페이스 제공 v5: 유연한 컨트롤러 어댑터 도입 어댑터를 추가해서 프레임워크를 유연하고 확장성 있게 설계
    졸라 어렵네 다시 한번 보자

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -4,19 +4,17 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SpringMemberSaveControllerV1 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @RequestMapping("/springmvc/v1/members/save")
-    public ModelAndView process(HttpServletRequest request) {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+    public ModelAndView process(@RequestParam String username, @RequestParam int age) {
+        //String username = request.getParameter("username");
+        //int age = Integer.parseInt(request.getParameter("age"));
 
         Member member = new Member(username, age);
         memberRepository.save(member);
---
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,42 @@
+package hello.servlet.web.springmvc.v2;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(@RequestParam String username, @RequestParam int age) {
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @GetMapping
+    public ModelAndView members() {
+        List<Member> memberList = memberRepository.findAll();
+        ModelAndView members = new ModelAndView("members");
+        members.getModel().put("members", memberList);
+        members.addObject("members", memberList);
+        return members;
+    }
+}
